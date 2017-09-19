7f3cf683946cc72b96ae6a5b700d679fa4e9d255
Brayan Rincon <brayan262@gmail.com>
2017-09-19 03:29:55 -0400

    Update centaurosms.php

--- lib/centaurosms.php
+++ lib/centaurosms.php
@@ -6,13 +6,15 @@
  * @autor Hernan Crespo
  *
  */
-class CentauroSMS {
+class CentauroSMS
+{
 
     private $client_id;
     private $client_secret;
     private $result_data;
 
-    function __construct($client_id, $client_secret) {
+    function __construct($client_id, $client_secret)
+    {
         $this->client_id = $client_id;
         $this->client_secret = $client_secret;
     }
@@ -20,18 +22,22 @@ class CentauroSMS {
     /**
      * Obtener numero de SMS disponibles
      */
-    public function get_sms_disponibles() {
+    public function get_sms_disponibles()
+    {
        $credenciales = $this->cParametros(array(
             'client_id' => $this->client_id,
             'client_secret' => $this->client_secret,
 			'client_opcion' => 'sms_disponibles'));
 
-       return $result_data = cSMSClient::post("/controllersms/", $credenciales, "application/x-www-form-urlencoded");		
+       $result_data = cSMSClient::post("/controllersms/", $credenciales, "application/x-www-form-urlencoded");
+	$result_data["response"]["sms_available"]
+	return $result_data
     }
     /**
      * Envio de SMS Masivos normales
      */    
-    public function set_sms_send($json,$msg) {
+    public function set_sms_send($json,$msg)
+    {
        $credenciales = $this->cParametros(array(
             'client_id' => $this->client_id,
             'client_secret' => $this->client_secret,
@@ -44,7 +50,8 @@ class CentauroSMS {
     /**
      * Envio de SMS Masivos personalizados
      */     	
-    public function set_sms_send_personalizado($json){
+    public function set_sms_send_personalizado($json)
+    {
        $credenciales = $this->cParametros(array(
             'client_id' => $this->client_id,
             'client_secret' => $this->client_secret,
@@ -53,7 +60,8 @@ class CentauroSMS {
 
        return $result_data = cSMSClient::post("/controllersms/", $credenciales, "application/x-www-form-urlencoded");     
     }  
-    private function cParametros($params) {
+    private function cParametros($params)
+    {
         if (function_exists("http_build_query")) {
             return http_build_query($params, "", "&");
         } else {
@@ -67,9 +75,10 @@ class CentauroSMS {
 }
 
 /**
- * CentauroSMS cURL Cliente
+ * CentSMS cURL Cliente
  */
-class cSMSClient{
+class cSMSClient
+{
 
     const API_BASE_URL = "http://api.centaurosms.com.ve";
 
@@ -80,26 +89,33 @@ class cSMSClient{
         curl_setopt($connect, CURLOPT_HTTPHEADER, array("Accept: application/json", "Content-Type: " . $content_type));
         return $connect;
     }
-    private static function set_data(&$connect, $data, $content_type) {
-        if ($content_type == "application/json") {
-            if (gettype($data) == "string") {
+    private static function set_data(&$connect, $data, $content_type)
+    {
+        if ($content_type == "application/json")
+	{
+            if (gettype($data) == "string")
+	    {
                 json_decode($data, true);
             } else {
                 $data = json_encode($data);
             }
 
-            if(function_exists('json_last_error')) {
+            if(function_exists('json_last_error'))
+	    {
                 $json_error = json_last_error();
-                if ($json_error != JSON_ERROR_NONE) {
+                if ($json_error != JSON_ERROR_NONE)
+		{
                     throw new Exception("JSON Error [{$json_error}] - Data: {$data}");
                 }
             }
         }
         curl_setopt($connect, CURLOPT_POSTFIELDS, $data);
     }	
-    private static function exec($method, $uri, $data, $content_type) {
+    private static function exec($method, $uri, $data, $content_type)
+    {
         $connect = self::get_connect($uri, $method, $content_type);
-        if ($data) {
+        if ($data)
+	{
             self::set_data($connect, $data, $content_type);
         }		
         $api_result = curl_exec($connect);
@@ -107,15 +123,18 @@ class cSMSClient{
         curl_close($connect);
         return $response;
     }
-    public static function get($uri, $content_type = "application/json") {
+    public static function get($uri, $content_type = "application/json")
+    {
         return self::exec("GET", $uri, null, $content_type);
     }
 
-    public static function post($uri, $data, $content_type = "application/json") {
+    public static function post($uri, $data, $content_type = "application/json")
+    {
         return self::exec("POST", $uri, $data, $content_type);
     }
 
-    public static function put($uri, $data, $content_type = "application/json") {
+    public static function put($uri, $data, $content_type = "application/json")
+    {
         return self::exec("PUT", $uri, $data, $content_type);
     }
 
